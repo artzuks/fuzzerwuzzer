3423b298b6145558129779fcf56778ba1b5f6e76
Art Zuks <zuks.art@gmail.com>
2019-03-10 18:01:28 -0400

    TCP handshake

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # fuzzerwuzzer
 Fuzz testing IP/TCP/App
+
+
+## Setup
+### Configuring OS
+Since scapy functions not within the kernel, for it to be able to establish TCP connections we must disable the kernel from sending RST packets.
+To do this, we can modify iptables to drop RST packets.
+
+```shell
+iptables -A OUTPUT -p tcp --tcp-flags RST RST -j DROP
+iptables -L
+```
+
--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from scapy.layers.inet import *
 from scapy.sendrecv import *
 
+
+
 if __name__ == '__main__':
-
-    packet = IP(dst="127.0.0.1") / TCP(dport=3000) / "Hi"
-    sr1(packet)
-
-
+    ip = IP(dst="45.55.124.203")
+    mes = "Hello from scapy"
+    dport = 3002
+    sport = 3000
+    SYN =  TCP(dport=dport, sport=sport, flags='S')
+    print ("Sending SYN")
+    SYNACK = sr1(ip/SYN)
+    ACK = TCP(dport=dport, sport=sport, flags='A', seq=SYNACK.ack, ack=SYNACK.seq + 1)
+    print ("Sending ACK")
+    R = send(ip / ACK)
+    DATA = TCP(dport=dport, sport=sport, flags="PA", seq=SYNACK.ack, ack=SYNACK.seq + 1)
+    print ("Sending Data")
+    DATASENT = sr1(ip/DATA/mes)
+    DATASENT.summary()
+    FIN = TCP(sport=sport, dport=dport, flags="FA", seq=SYNACK.ack + 2, ack=SYNACK.seq + 2)
+    print ("Sending FIN")
+    FINACK = sr1(ip/FIN)
+    LASTACK = TCP(sport=sport, dport=dport, flags="A", seq=FINACK.ack, ack=FINACK.seq + 1)
+    print ("Sending FINACK")
+    send(ip/LASTACK)
