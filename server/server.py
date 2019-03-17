7b4146fdc254de90abe46bc6b6a3d5d83162fdcd
Art Zuks <zuks.art@gmail.com>
2019-03-17 18:27:09 +0000

    initial pattern match count

--- server/server.py
+++ server/server.py
@@ -3,41 +3,62 @@ import sys
 import signal
 
 
-def sigterm_handler(_signo, _stack_frame):
-    # Raises SystemExit(0):
-    sock.close()
-    sys.exit(0)
-
-matchPattern = ''
+matchPattern = bytearray()
 MAX_PAYLOAD_LIMIT = 1024
+matchedPayloads = 0
+totalPayloads = 0
+
 def readMatchPatternFromFile(path):
     """
-    The function reads the first line of the given file
-    and sets the matchPattern to that value for the duration of 
-    the service uptime. 
+    The function reads the given file and sets the matchPattern
+    to that value for the duration of the service uptime. 
 
     If the pattern exceeds the MAX_PAYLOAD_LIMIT the function will
     throw an exception.
     """
     with open(path,'r') as fp:
-        matchPattern = fp.readline()
+        matchPattern = bytearray.fromhex(fp.read().strip())
         if len(matchPattern) > MAX_PAYLOAD_LIMIT:
             raise Exception('Pattern for matching exceeds {}'.format(MAX_PAYLOAD_LIMIT))
         elif len(matchPattern) == 0:
             raise Exception('Pattern for matching cannot be 0 in length')
 
 
+def checkPayloadForPattern(b):
+    if len(b) < len(matchPattern):
+        return False
+
+    for i in range(len(matchPattern)):
+        if matchPattern[i] != b[i]:
+            return False
+
+    return True
+     
+
+
+
 readMatchPatternFromFile('./match_pattern')
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 # Bind the socket to the port
 server_address = ('0.0.0.0', 3000)
 print('starting up on {} port {}'.format(*server_address))
 sock.bind(server_address)
 
 # Listen for incoming connections
-sock.listen(3)
+sock.listen(1)
+
+
+def sigterm_handler(_signo, _stack_frame):
+    # Raises SystemExit(0):
+    print('{} valid payloads'.format(matchedPayloads))
+    print('{} invalid payloads'.format(totalPayloads-matchedPayloads))
+    sock.close()
+    sys.exit(0)
+
+
+
 signal.signal(signal.SIGINT, sigterm_handler)
 signal.signal(signal.SIGTERM, sigterm_handler)       
     
@@ -48,18 +69,22 @@ while True:
     # Wait for a connection
     print('waiting for a connection')
     connection, client_address = sock.accept()
-    signal.signal(signal.SIGINT, connection.close)
-    signal.signal(signal.SIGTERM, connection.close)       
     try:
         print('connection from', client_address)
 
         # Receive the data in small chunks and retransmit it
+        buff = memoryview(bytearray(MAX_PAYLOAD_LIMIT))
         while True:
-            data = connection.recv(1024)
-            print('received {!r}'.format(data))
-            if data:
-                print('sending data back to the client')
-                #connection.sendall(data)
+            
+            numRecv = connection.recv_into(buff,MAX_PAYLOAD_LIMIT)
+            if numRecv:
+                print(bytes(buff[0:numRecv]))
+                totalPayloads += 1
+                if checkPayloadForPattern(buff):
+                    matchedPayloads += 1
+                    connection.sendall(bytearray.fromhex("00"))
+                else:
+                    connection.sendall(bytearray.fromhex("ff"))
             else:
                 print('no data from', client_address)
                 break
